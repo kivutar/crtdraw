a2cfbb2789efe6326ac382a5159fdcf32fd38821
Jean-André Santoni <jean.andre.santoni@gmail.com>
2023-08-16 23:17:12 +0200

    Overlay

--- main.c
+++ main.c
@@ -44,11 +44,12 @@ void set_pixel(
 	const int y,
 	const Uint8 r,
 	const Uint8 g,
-	const Uint8 b)
+	const Uint8 b,
+	const Uint8 a)
 {
 	Uint32 *const pixels = (Uint32 *) surface->pixels;
 
-	const Uint32 color = SDL_MapRGB(surface->format, r, g, b);
+	const Uint32 color = SDL_MapRGBA(surface->format, r, g, b, a);
 
 	pixels[x + (y * surface->w)] = color;
 }
@@ -85,19 +86,26 @@ int main(void) {
 	if (window == NULL)
 		die("SDL_CreateWindow failed: %s\n", SDL_GetError());
 
-	SDL_Surface *const spr_srf = SDL_CreateRGBSurface(0, spr_w, spr_h, 32, 0, 0, 0, 0);
-	SDL_FillRect(spr_srf, NULL, SDL_MapRGB(spr_srf->format, 255, 255, 255));
+	// sprite
+	SDL_Surface *const spr_srf = SDL_CreateRGBSurface(0, spr_w, spr_h, 32, 0x000000FF, 0x0000FF00, 0x00FF0000, 0xFF000000);
+	SDL_FillRect(spr_srf, NULL, SDL_MapRGBA(spr_srf->format, 255, 255, 255, 255));
+	SDL_SetSurfaceBlendMode(spr_srf, SDL_BLENDMODE_BLEND);
 
+	// overlay
+	SDL_Surface *const ovl_srf = SDL_CreateRGBSurface(0, spr_w, spr_h, 32, 0x000000FF, 0x0000FF00, 0x00FF0000, 0xFF000000);
+	SDL_FillRect(ovl_srf, NULL, SDL_MapRGBA(ovl_srf->format, 0, 0, 0, 0));
+	SDL_SetSurfaceBlendMode(ovl_srf, SDL_BLENDMODE_BLEND);
+
+	// window
 	SDL_Surface *win_srf = SDL_GetWindowSurface(window);
+	SDL_FillRect(win_srf, NULL, SDL_MapRGBA(win_srf->format, 128, 128, 128, 255));
+	SDL_SetSurfaceBlendMode(win_srf, SDL_BLENDMODE_BLEND);
 
 	if (win_srf == NULL)
 		die("SDL_GetWindowSurface failed: %s\n", SDL_GetError());
 
     SDL_Cursor* cursor = load_png_cursor("pointer.png", 0, 0);
     SDL_SetCursor(cursor);
-
-	SDL_FillRect(win_srf, NULL, SDL_MapRGB(win_srf->format, 128, 128, 128));
-	SDL_UpdateWindowSurface(window);
 
 	int mouse_x = 0;
 	int mouse_y = 0;
@@ -140,21 +148,28 @@ int main(void) {
 			}
 		}
 
-		if (drawing) {
-			x = clamp(x, screen_w / 2 - spr_w*8 / 2, screen_w / 2 + spr_w*8 / 2 - 1);
-			y = clamp(y, screen_h / 2 - spr_h*8 / 2, screen_h / 2 + spr_h*8 / 2 - 1);
+		int offset_x = screen_w / 2 - spr_w*8 / 2;
+		int offset_y = screen_h / 2 - spr_h*8 / 2;
 
-			set_pixel(spr_srf, (x - screen_w/2 + spr_w*8/2) / 8 , (y - screen_h/2 + spr_h*8/2) / 8 , r, g, b);
-		}
+		x = clamp(x, offset_x, screen_w / 2 + spr_w*8 / 2 - 1);
+		y = clamp(y, offset_y, screen_h / 2 + spr_h*8 / 2 - 1);
 
-		// Scaled sprite
+		if (drawing)
+			set_pixel(spr_srf, (x - offset_x) / 8 , (y - offset_y) / 8 , r, g, b, 255);
+
+		// Scaled sprite with border
 		SDL_Rect src_rect = { 0, 0, spr_w, spr_h };
-		SDL_Rect dst_rect = { screen_w / 2 - spr_w*8 / 2, screen_h / 2 - spr_h*8 / 2, spr_w*8, spr_h*8 };
-		SDL_Rect dst_rect2 = { screen_w / 2 - spr_w*8 / 2 - 1, screen_h / 2 - spr_h*8 / 2 - 1, spr_w*8 + 2, spr_h*8 + 2 };
+		SDL_Rect dst_rect = { offset_x, offset_y, spr_w*8, spr_h*8 };
+		SDL_Rect border_rect = { offset_x - 1, offset_y - 1, spr_w*8 + 2, spr_h*8 + 2 };
 
-		SDL_FillRect(win_srf, &dst_rect2, SDL_MapRGB(win_srf->format, 64, 64, 64));
+		SDL_FillRect(win_srf, &border_rect, SDL_MapRGBA(win_srf->format, 64, 64, 64, 255));
+
+		// Overlay
+		SDL_FillRect(ovl_srf, NULL, SDL_MapRGBA(ovl_srf->format, 0, 0, 0, 0));
+		set_pixel(ovl_srf, (x - offset_x) / 8 , (y - offset_y) / 8 , 255, 0, 0, 128);
 
 		SDL_BlitScaled(spr_srf, &src_rect, win_srf, &dst_rect);
+		SDL_BlitScaled(ovl_srf, &src_rect, win_srf, &dst_rect);
 
 		// Miniature
 		SDL_Rect dst_rect3 = { screen_w - spr_w - 8, screen_h - spr_h - 8, spr_w, spr_h };
